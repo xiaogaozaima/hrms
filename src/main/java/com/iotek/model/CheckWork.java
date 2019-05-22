7e54a695e7732851052ee5158672152a6a249db5
xiaogaozaima <xiaogaozaima@126.com>
2019-05-22 09:36:39 +0800

    正在做： 员工考勤打卡,目前已实现（没进行迟到旷工判断）上下班打卡

--- src/main/java/com/iotek/model/CheckWork.java
+++ src/main/java/com/iotek/model/CheckWork.java
@@ -6,6 +6,9 @@ public class CheckWork implements Serializable {
     private Integer che_id;
     private String che_onTime;
     private String che_offTime;
+    private Integer che_staff_id;
+    private String che_ym;
+    private String che_date;
 
     public Integer getChe_id() {
         return che_id;
@@ -31,12 +34,39 @@ public class CheckWork implements Serializable {
         this.che_offTime = che_offTime;
     }
 
+    public Integer getChe_staff_id() {
+        return che_staff_id;
+    }
+
+    public void setChe_staff_id(Integer che_staff_id) {
+        this.che_staff_id = che_staff_id;
+    }
+
+    public String getChe_ym() {
+        return che_ym;
+    }
+
+    public void setChe_ym(String che_ym) {
+        this.che_ym = che_ym;
+    }
+
+    public String getChe_date() {
+        return che_date;
+    }
+
+    public void setChe_date(String che_date) {
+        this.che_date = che_date;
+    }
+
     @Override
     public String toString() {
         return "CheckWork{" +
                 "che_id=" + che_id +
                 ", che_onTime='" + che_onTime + '\'' +
                 ", che_offTime='" + che_offTime + '\'' +
+                ", che_staff_id=" + che_staff_id +
+                ", che_ym='" + che_ym + '\'' +
+                ", che_date='" + che_date + '\'' +
                 '}';
     }
 }
